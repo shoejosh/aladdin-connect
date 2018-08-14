ca8334f94cf47831dcb5c2ae2973a1fd0414d7df
Josh Shoemaker <shoejosh@gmail.com>
2018-08-13 20:23:42 -0400

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='aladdin_connect',
-    version='0.1',
+    version='0.2',
     author='Josh Shoemaker',
     author_email='shoejosh@gmail.com',
     url='http://github.com/shoejosh/aladdin-connect',
